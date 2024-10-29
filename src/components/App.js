501b392ca0480583c28c7e8dfa155787e9894714
Harshal39 <harshalbhute39@gmail.com>
2024-10-29 14:47:09 +0530

    Commits

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,11 @@ const App = () => {
   const [first, setfirst] = useState(0);
   return (
     <div>
+        <p>Button click {first} times</p>
         {/* Do not remove the main div */}
         <button onClick = {() =>{
           setfirst(first + 1);
         }}>Click Me</button>
-        <p>Button click {first} times</p>
     </div>
   )
 }
