71a19d3efea96ebefe8b18641b187485ad8d2635
Harshal39 <harshalbhute39@gmail.com>
2024-10-29 14:41:59 +0530

    Committed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [first, setfirst] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick = {() =>{
+          setfirst(first + 1);
+        }}>Click Me</button>
+        <p>Button click {first} times</p>
     </div>
   )
 }
